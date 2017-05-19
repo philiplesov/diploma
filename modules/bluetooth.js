5e54373ae8482a9ef4f4ec6a6574c49c03632d73
philiplesov <meyzec@abv.bg>
2017-05-19 19:19:31 +0300

    added splitting the gps data with remainder logic

--- modules/bluetooth.js
+++ modules/bluetooth.js
@@ -3,19 +3,34 @@ var webSocket = require('./webSocket.js');
 
 exports.btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
 
+var remainder;
+
 //Bluetooth
 exports.btSerial.on('found', function(address, name) {
     console.log(address, name);
     exports.btSerial.findSerialPortChannel(address, function(channel) {
         exports.btSerial.connect(address, channel, function() {
             console.log('connected');
-            var buffer = Buffer.alloc(10);
+
             exports.btSerial.on('data', function(buffer) {
-                console.log(buffer.toString('utf-8'));
+            	var data = buffer.toString('utf-8');
+            	console.log('data:', buffer.toString('utf-8'));
+            	if (remainder) {
+            		data = remainder + data;
+            		remainder = '';
+            	}
+            	console.log('data+remainder:', data);
+            	var res = data.split("$");
+            	console.log('res: ', res);
+            	expr = /\r?\n|\r/;
+                if (res.length > 0 && !expr.test(res[res.length-1])) {
+                	remainder = res.pop();
+                }
+                console.log('remainder: ', remainder);
                 console.log('=================');
-                webSocket.saveLatestData(buffer.toString('utf-8'));
-                dbInsert = require('./modules/database/insertData.js');
-				dbInsert.insert(buffer.toString('utf-8'));
+                //webSocket.saveLatestData(buffer.toString('utf-8'));
+                //dbInsert = require('./database/insertData.js');
+				//dbInsert.insert(buffer.toString('utf-8'));
             });
         }, function () {
             console.log('cannot connect');
